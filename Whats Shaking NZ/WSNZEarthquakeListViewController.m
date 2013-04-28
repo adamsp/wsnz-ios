afe36400bedac5b7a45a132abff3d5cc85318d4e
Adam Speakman <adam.speakman@gmail.com>
2013-04-28 15:34:21 +1200

    List view changes
    Refresh button now standard icon
    Loading quakes on startup
    Displaying dates as well as magnitude (need to build a custom view)

--- Whats Shaking NZ/WSNZEarthquakeListViewController.m
+++ Whats Shaking NZ/WSNZEarthquakeListViewController.m
@@ -31,37 +31,13 @@
     [self.model requestUpdate];
 }
 
-- (id) init
-{
-    self = [super init];
-    if (self)
-    {
-
-        [self.model requestUpdate];
-    }
-    return self;
-}
-
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        self.model = [WSNZEarthquakeModel instance];
-        [self.model addUpdateListener:self];
-        [self.model requestUpdate];
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.model = [WSNZEarthquakeModel instance];
+    [self.model addUpdateListener:self];
+    [self.model requestUpdate];
 }
 
 - (void)didReceiveMemoryWarning
@@ -84,51 +60,14 @@
     static NSString *CellIdentifier = @"Earthquake Description";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-    // Configure the cell...
+    // TODO Should be using a custom view
     Earthquake *earthquake = [[self.model quakes] objectAtIndex:indexPath.row];
     cell.textLabel.text = [NSString stringWithFormat:@"%.1f", [earthquake.magnitude doubleValue]];
+    // TODO Should be using NSDateFormatter
+    cell.detailTextLabel.text = [earthquake.date description];
     
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
